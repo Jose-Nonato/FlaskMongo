62407cc5f3e997b9650b5cc99fba87cf59855033
JoseJunior <beijallote@gmail.com>
2022-07-26 17:43:47 -0300

    feat: conection with database

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template
+import pymongo
 
 app = Flask(__name__)
 
+try:
+    mydb = pymongo.MongoClient("mongodb://localhost:27017/")
+    db = mydb["CrudFlask"]
+    print("Conectado")
+except:
+    print("Tente novamente")
+
 @app.route("/")
 def Initial():
-    return "Hello World"
+    return render_template("index.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
